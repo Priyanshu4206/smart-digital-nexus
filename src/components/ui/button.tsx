3ccadeb4b5fb1d5c464cf0176e8286678898fbbf
Priyanshu4206 <agarwalpriyanshu168@gmail.com>
2025-03-21 02:17:11 +0530

    pushed code

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -64,7 +64,7 @@ const buttonVariants2 = cva(
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
-    VariantProps<typeof buttonVariants> {
+  VariantProps<typeof buttonVariants> {
   asChild?: boolean
 }
 
